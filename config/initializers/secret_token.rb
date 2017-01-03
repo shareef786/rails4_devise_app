777d940a30c103d7dd7e07696338150206debf3f
shareef <shareef.ddkl786@gmail.com>
2017-01-03 08:13:30 +0530

    Adding my app to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4DeviseApp::Application.config.secret_key_base = 'c593962032db8acc97145a6362bc946b114d6a4a0f71473dfc19a09c3cd307b8d37013a49fce589cc747dfefba3c3619c3802168631738001fae87eeb2c7e533'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails4DeviseApp::Application.config.session_store :cookie_store, key: '_rails4_devise_app_session'
